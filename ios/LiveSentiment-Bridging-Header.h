f7e1d79278c4b3bb905bb74e194e1a2d4469f5bf
Danial Khosravi <dani_khosravi@yahoo.com>
2018-03-11 19:20:24 +1100

    Finally model is being recognised by the code. Need to finish final gluing

--- ios/LiveSentiment-Bridging-Header.h
+++ ios/LiveSentiment-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import <React/RCTBridgeModule.h>
+#import "SentimentModel.h"
--- ios/Sentiment.swift
+++ ios/Sentiment.swift
@@ -6,43 +6,43 @@
 //  Copyright © 2018 Facebook. All rights reserved.
 //
 
-//import CoreML
+import Foundation
+import CoreML
 
 @objc(Sentiment)
 class Sentiment: NSObject {
-  let model = SentimentModel()
   @objc static func requiresMainQueueSetup() -> Bool {
     return false
   }
   
   @objc(check:callback:)
   func check(text: [NSNumber], callback: RCTResponseSenderBlock) {
-    print("checking", text.count)
-    
     if #available(iOS 11.0, *) {
-      //      guard let input_data = try? MLMultiArray(shape:[1000,1,1], dataType:.double) else {
-      //        fatalError("Unexpected runtime error. MLMultiArray")
-      //      }
-      //      for (index, item) in text.enumerated() {
-      //        input_data[index] = item
-      //      }
-      //      let i = SentimentModelInput(input: input_data)
-      //      let model = Split
-      //      let sentiment_prediction = try model.prediction(input: i)
-      //      if(sentiment_prediction.sentiment[0].doubleValue >= 0.5) {
-      //        print("positive!")
-      //      } else {
-      //        print("negative!")
-      //      }
-      //      self.query()
+      let model = SentimentModel()
+
+      guard let input_data = try? MLMultiArray(shape:[1000, 1, 1], dataType:.double) else {
+        fatalError("Unexpected runtime error. MLMultiArray")
+      }
+
+      for (index, item) in text.enumerated() {
+        input_data[index] = item
+      }
       
-      //      let sentiment_prediction = try model.prediction(input: text)
-      //      if (sentiment_prediction) {
-      //        print(sentiment_prediction)
-      //      }
+      let i = SentimentModelInput(input: input_data)
+
+      let sentiment_prediction = try! model.prediction(fromFeatures: i)
+      print(sentiment_prediction.output.count)
+      print(sentiment_prediction.output[0]?.doubleValue)
+//      if(sentiment_prediction.output[0]?.doubleValue >= 0.5) {
+//        print("positive!")
+//      } else {
+//        print("negative!")
+//      }
+
+      
+      
       let probability = Float(Float(arc4random()) / Float(UINT32_MAX))
       callback([NSNull(), probability])
-      
     } else {
       print("NATT")
     }
